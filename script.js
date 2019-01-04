6e9df8eab570a0ccd7ef6a94a4163cfb37e1382f
Tiffany Phan <Tiffany.Phan@colorado.edu>
2019-01-03 19:02:40 -0700

    Messing around with scrolling via jQuery

---
+++ script.js
@@ -0,0 +1,22 @@
+var foo = this
+
+$.fn.followTo = function ( pos ) {
+    var $this = this,
+        $window = $(foo);
+    
+    $window.scroll(function(e){
+        if ($window.scrollTop() > pos) {
+            $this.css({
+                position: 'absolute',
+                top: pos
+            });
+        } else {
+            $this.css({
+                position: 'fixed',
+                top: 0
+            });
+        }
+    });
+};
+
+$('#title').followTo(100);
