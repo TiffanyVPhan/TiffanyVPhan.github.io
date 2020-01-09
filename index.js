38e75e356f2c03df2fb65b4102a48cb7d46eb211
TiffanyVPhan <tiffany.phan@colorado.edu>
2020-01-08 23:58:07 -0700

    added icons

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    M.Parallax.init(elems, {});
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems, {});
+});
