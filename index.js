3eb25cdc044f562ff49eef1c1a255c2d6c07c094
TiffanyVPhan <tiffany.phan@colorado.edu>
2020-06-01 15:41:39 -0600

    improved card ui and cleaned up css classes

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    M.Parallax.init(elems, {});
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(elems, {});
-});
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, {});
+  });     
