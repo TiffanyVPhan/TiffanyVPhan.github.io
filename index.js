85beb9e7ad76f60228405e31ad47dcb928d59e89
TiffanyVPhan <tiffany.phan@colorado.edu>
2020-07-16 18:36:29 -0600

    working scrollmagic

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, {});
-  });     
+let controller = new ScrollMagic.Controller({
+  globalSceneOptions: {
+    triggerHook: 'onLeave',
+    duration: '200%' 
+  }
+});
+
+let scene = new ScrollMagic.Scene({
+  triggerElement: '.bg-2'
+})
+.setClassToggle('.bg-2', 'show')
+.addTo(controller);
+
+
+let slides = document.querySelectorAll('section.panel');
+
+for (let i=0; i<slides.length; i++) {
+  new ScrollMagic.Scene({
+      triggerElement: slides[i]
+    })
+    .setPin(slides[i], {pushFollowers: false})
+    .addIndicators() 
+    .addTo(controller);
+}
+
+let arrowID = document.getElementById('arrow1');
+
+let myScrollFunc = function () {
+  let y = window.scrollY;
+  if (y >= 800) {
+    arrowID.classList = "arrow hide"
+  } else {
+    arrowID.classList = "arrow show"
+  }
+};
+
+window.addEventListener("scroll", myScrollFunc);
